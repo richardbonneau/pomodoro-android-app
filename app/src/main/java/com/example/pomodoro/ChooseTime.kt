82b5f4210ffe3618c1e640d79cfb69bee10656d8
richardbonneau <richardbonneau@hotmail.com>
2019-05-17 20:17:39 -0400

    Added activity

---
+++ app/src/main/java/com/example/pomodoro/ChooseTime.kt
@@ -0,0 +1,12 @@
+package com.example.pomodoro
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseTime : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_time)
+    }
+}
